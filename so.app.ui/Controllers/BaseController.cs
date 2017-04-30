ba46ca5512037338708d46fbad33c5bd7dbdafdc
humo <huangdinghe@hotmail.com>
2017-04-30 11:39:58 +0800

    add base user controller and users entity

---
+++ so.app.ui/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace so.app.ui.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+
+    }
+}
